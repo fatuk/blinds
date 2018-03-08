e7e6e646171f7e7cd425b01bb1d343ef97c3bd80
Sofa Fatuk <sofa111298@gmail.com>
2018-03-08 22:34:53 +0300

    Добавила выплывающее окно в single-product

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 $(() => {
-	console.log('Hello ES2015!');
+	$('.js-hover').on('mouseover', (e) => {
+		const $target = $(e.currentTarget);
+		$target.addClass($target.data('class'));
+	});
+
+	$('.js-hover').on('mouseout', (e) => {
+		const $target = $(e.currentTarget);
+		$target.removeClass($target.data('class'));
+	});
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,14 @@
 'use strict';
 
 $(function () {
-	console.log('Hello ES2015!');
+	$('.js-hover').on('mouseover', function (e) {
+		var $target = $(e.currentTarget);
+		$target.addClass($target.data('class'));
+	});
+
+	$('.js-hover').on('mouseout', function (e) {
+		var $target = $(e.currentTarget);
+		$target.removeClass($target.data('class'));
+	});
 });
 //# sourceMappingURL=../js/app.js.map
