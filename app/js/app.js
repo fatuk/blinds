bad52d7c550a81a15333edebf668c70579d45218
Sofa Fatuk <sofa111298@gmail.com>
2018-03-08 23:22:05 +0300

    Добавлено всплывающее меню

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 $(() => {
+	// Mouse hover
 	$('.js-hover').on('mouseover', (e) => {
 		const $target = $(e.currentTarget);
 		$target.addClass($target.data('class'));
@@ -7,5 +8,15 @@ $(() => {
 	$('.js-hover').on('mouseout', (e) => {
 		const $target = $(e.currentTarget);
 		$target.removeClass($target.data('class'));
+	});
+	
+	// Hide on scroll
+	const $hideOnScroll = $('[data-scroll-class]');
+	$(window).on('scroll', () => {
+		if ($(window).scrollTop() >= 330) {
+			$hideOnScroll.addClass($hideOnScroll.data('scroll-class'));
+		} else {
+			$hideOnScroll.removeClass($hideOnScroll.data('scroll-class'));
+		}
 	});
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(function () {
+	// Mouse hover
 	$('.js-hover').on('mouseover', function (e) {
 		var $target = $(e.currentTarget);
 		$target.addClass($target.data('class'));
@@ -9,6 +10,16 @@ $(function () {
 	$('.js-hover').on('mouseout', function (e) {
 		var $target = $(e.currentTarget);
 		$target.removeClass($target.data('class'));
+	});
+
+	// Hide on scroll
+	var $hideOnScroll = $('[data-scroll-class]');
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() >= 330) {
+			$hideOnScroll.addClass($hideOnScroll.data('scroll-class'));
+		} else {
+			$hideOnScroll.removeClass($hideOnScroll.data('scroll-class'));
+		}
 	});
 });
 //# sourceMappingURL=../js/app.js.map
